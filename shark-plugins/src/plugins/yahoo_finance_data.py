4b5a02e1db85242685b3af12d9c42bbec6450d5a
Daniel Neil <danielneilhodder@gmail.com>
2021-08-20 10:37:49 +1000

    Update yahoo_finance_data.py

--- shark-plugins/src/plugins/yahoo_finance_data.py
+++ shark-plugins/src/plugins/yahoo_finance_data.py
@@ -1,1 +1,7 @@
+import requests
 
+url = 'https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1597883791&period2=1629419791&interval=1d&events=history&includeAdjustedClose=true'
+
+r = requests.get(url, allow_redirects=True)
+
+open('facebook.ico', 'wb').write(r.content)
