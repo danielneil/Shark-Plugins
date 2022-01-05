baca3f081d27dc8bf2b09a5950a770b8e7136de5
Daniel Neil <danielneilhodder@gmail.com>
2022-01-05 18:36:10 +1100

    Update yahoo_finance_data.py

--- yahoo_finance_data/files/yahoo_finance_data.py
+++ yahoo_finance_data/files/yahoo_finance_data.py
@@ -15,15 +15,39 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=cmd_arg_help)
     parser.add_argument("-t", "--ticker", help="Ticker code of the desired instrument")
+    parser.add_argument("-p1", "--period1", help="Data start date")
+    parser.add_argument("-p2", "--period2", help="Data end date")
+    parser.add_argument("-i", "--interval", help="Time interval of the data")
+    parser.add_argument("-a", "--includeAdjustedClose", help="includeAdjustedClose [TRUE|FALSE] ")
     args = parser.parse_args()
 
     if not args.ticker:
         print ("UNKNOWN - No ticker specified")
         sys.exit(UNKNOWN)
 
+    if not args.period1:
+        print ("UNKNOWN - No period1 specified")
+        sys.exit(UNKNOWN)
+        
+    if not args.period2:
+        print ("UNKNOWN - No period2 specified")
+        sys.exit(UNKNOWN)
+    
+    if not args.interval:
+        print ("UNKNOWN - No interval specified")
+        sys.exit(UNKNOWN) 
+        
+    if not args.includeAdjustedClose:
+        print ("UNKNOWN - No includeAdjustedClose specified")
+        sys.exit(UNKNOWN) 
+        
     ticker = args.ticker 
-
-    url = 'https://query1.finance.yahoo.com/v7/finance/download/BTC-USD?period1=1609831036&period2=1641367036&interval=1d&events=history&includeAdjustedClose=true'
-    urllib.request.urlretrieve(url, "/shark/historical/yahoo_finance/BTC-USD")
+    period1 = args.period1
+    period2 = args.period2
+    interval = args.interval
+    includeAdjustedClose = args.includeAdjustedClose
+    
+    url = 'https://query1.finance.yahoo.com/v7/finance/download/'+ticker+'?period1='+period1+'&period2='+period2+'&interval='+interval+'&events=history&includeAdjustedClose='+includeAdjustedClose
+    urllib.request.urlretrieve(url, "/shark/historical/yahoo_finance/" + ticker)
 
     sys.exit(OK)
