9aa3ccec73a402f9235cc650803eab82074e02be
Daniel Neil <danielneilhodder@gmail.com>
2022-01-09 15:40:03 +1100

    Update sma.py

--- sma/files/sma.py
+++ sma/files/sma.py
@@ -19,8 +19,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=cmd_arg_help)
     parser.add_argument("-t", "--ticker", help="Ticker code of the stock.")
     parser.add_argument("-p", "--periods", help="Number of trading periods for which to compute against.")
-    parser.add_argument("-max", "--max", help="Warn if the result is greater than this threshold.")
-    parser.add_argument("-min", "--min", help="Warn if the result is less than this threshold.")
     parser.add_argument("-f", "--provider", help="Which provider to the histocal data come from (e.g. yahoo_finance)")
     args = parser.parse_args()
 
@@ -49,22 +47,28 @@ if __name__ == "__main__":
 
     history_dir = "/shark/historical/"
 
-    if args.provider == "yahoo_finance":
+    if args.provider == "yahoo_finance_data":
         datafile = history_dir + args.provider
         
     data = pd.read_csv(datafile + "/" + ticker + ".csv")
+    
     dataFrame = data['Adj Close']
+    
     sma = np.round(dataFrame.rolling(smaPeriod).mean().iloc[-1], 2)
 
     if args.raw:
         print(str(sma))
         sys.exit(OK)
+        
     else:
+        
         if args.max and int(args.max) <  sma:
             print("WARNING - SMA($" + str(sma) + ") is above threshold " + str(args.max))
             sys.exit(WARNING)
+            
         elif args.min and int(args.min) >  sma:
             print("WARNING - SMA($" + str(sma) + ") is below threshold " + str(args.min))
             sys.exit(WARNING)
+            
         else:
             print("OK - $" + str(sma))
