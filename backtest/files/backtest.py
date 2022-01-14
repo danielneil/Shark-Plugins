5c566273380a3560760af6f13ac43c3d00f6aa3e
Daniel Neil <danielneilhodder@gmail.com>
2022-01-15 00:07:57 +1100

    Update backtest.py

--- backtest/files/backtest.py
+++ backtest/files/backtest.py
@@ -10,11 +10,9 @@ WARNING      = 1
 CRITICAL     = 2
 UNKNOWN      = 3
 
-cmd_arg_help = 'This executes the backtest code. For a simple backtest example, see the template'
-
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description=cmd_arg_help)
+    parser = argparse.ArgumentParser(fromfile_prefix_chars='@')
     parser.add_argument("-b", "--backTestFile", help="The name of the file containing the back test code.")
     parser.add_argument("-s", "--scriptFile", help="A script file containing the commands which are used to build the backtest command line arguments.")
     args = parser.parse_args()
