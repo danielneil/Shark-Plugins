10c7d41335e3cf449de33ab20a2f868a3aadf9fc
Daniel Neil <danielneilhodder@gmail.com>
2022-01-15 20:22:13 +1100

    Update ema.py

--- ema/files/ema.py
+++ ema/files/ema.py
@@ -12,11 +12,9 @@ WARNING      = 1
 CRITICAL     = 2
 UNKNOWN      = 3
 
-cmd_arg_help = "This plugin checks the exponential moving average (ema) for a stock. Alerts Green (OK) if the share price is above the ema, Yellow(Warning) if its below"
-
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description=cmd_arg_help)
+    parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--ticker", help="Ticker code of the stock.")
     parser.add_argument("-p", "--periods", help="Number of trading periods for which to compute against.")
     parser.add_argument("-f", "--provider", help="Which provider to the histocal data come from (e.g. yahoo_finance)")
